7f5e07317e2bdc6e10a22cdc3a04e1db6e40dabc
Jonathan Scherman <jonischerman@gmail.com>
2016-06-04 14:30:30 -0300

    Revert "Ya compila"
    This reverts commit 6697e2c0a094c3d88506698422f45359454d590f.

--- aux.h
+++
@@ -1,9 +0,0 @@
-#ifndef AUX_H
-#define AUX_H
-
-#include "tipos.h"
-
-//Declaren aqui sus funciones auxiliares globales...
-std::ostream & operator<<(std::ostream & os, const Posicion & p);
-
-#endif //AUX_H
